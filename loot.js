4258c625a310e57ac1602fbc57bbeb71c599b948
Climer00 <Climer00@gmail.com>
2026-09-06 11:26:28 -0500

    fix: correct loot.js CDN shim script close tag

--- loot.js
+++ loot.js
@@ -1,2 +1,2 @@
 /* Temporary shim: load last known-good loot.js until expanded copy is restored */
-document.write('<script src="https://cdn.jsdelivr.net/gh/Climer00/table-loot-forge@12e1f6c3f6dca2e7aa0ca177317caf8b586a3152/loot.js"><\\/script>');
+document.write('<script src="https://cdn.jsdelivr.net/gh/Climer00/table-loot-forge@12e1f6c3f6dca2e7aa0ca177317caf8b586a3152/loot.js"><\/script>');
